6f7def17c9353237d885b45f586eef646b074c4e
Marco <mass7813@gmail.com>
2023-08-04 20:20:33 -0300

    First commit

---
+++ src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+import application.model.entites.Account;
+import application.model.exceptions.DomainExceptions;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Holder: ");
+            String holder = sc.nextLine();
+            System.out.print("Initial balance: ");
+            Double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: ");
+            Double withdrawLimit = sc.nextDouble();
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            System.out.print("Enter amount for withdraw: ");
+            Double withdraw = sc.nextDouble();
+            account.withdraw(withdraw);
+            System.out.printf("New balance: %.2f", account.getBalance());
+        }
+        catch (DomainExceptions e) {
+            System.out.println("Withdraw error: "+ e.getMessage());
+        }
+        catch (RuntimeException e) {
+            System.out.println("Unexpect Error!");
+        }
+    }
+}
